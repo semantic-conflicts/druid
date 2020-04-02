ab39c2fa92ab6654bb72a34335c6f7750dca0dd8
leusonmario <leusonmario@gmail.com>
2020-04-02 13:04:23 -0300

    'Trigger build #4f70bbc687'

--- server/src/main/java/com/metamx/druid/loading/S3SegmentPusher.java
+++ server/src/main/java/com/metamx/druid/loading/S3SegmentPusher.java
@@ -1,24 +1,4 @@
-/*
- * Druid - a distributed column store.
- * Copyright (C) 2012  Metamarkets Group Inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 package com.metamx.druid.loading;
-
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Closeables;
@@ -33,116 +13,76 @@ import org.jets3t.service.S3ServiceException;
 import org.jets3t.service.acl.gs.GSAccessControlList;
 import org.jets3t.service.impl.rest.httpclient.RestS3Service;
 import org.jets3t.service.model.S3Object;
-
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-public class S3SegmentPusher implements SegmentPusher
-{
-  private static final EmittingLogger log = new EmittingLogger(S3SegmentPusher.class);
-  private static final Joiner JOINER = Joiner.on("/").skipNulls();
-
-  private final RestS3Service s3Client;
-  private final S3SegmentPusherConfig config;
-  private final ObjectMapper jsonMapper;
-
-  public S3SegmentPusher(
-    RestS3Service s3Client,
-    S3SegmentPusherConfig config,
-    ObjectMapper jsonMapper
-  )
-  {
-    this.s3Client = s3Client;
-    this.config = config;
-    this.jsonMapper = jsonMapper;
+public class S3SegmentPusher implements SegmentPusher {
+  public static EmittingLogger log=new EmittingLogger(S3SegmentPusher.class);
+  public static Joiner JOINER=Joiner.on("/").skipNulls();
+  public RestS3Service s3Client;
+  public S3SegmentPusherConfig config;
+  public ObjectMapper jsonMapper;
+  public S3SegmentPusher(  RestS3Service s3Client,  S3SegmentPusherConfig config,  ObjectMapper jsonMapper){
+    this.s3Client=s3Client;
+    this.config=config;
+    this.jsonMapper=jsonMapper;
   }
-
-  @Override
-  public DataSegment push(File file, DataSegment segment) throws IOException
-  {
-    log.info("Uploading [%s] to S3", file);
-    String outputKey = JOINER.join(
-        config.getBaseKey().isEmpty() ? null : config.getBaseKey(),
-        segment.getDataSource(),
-        String.format(
-            "%s_%s",
-            segment.getInterval().getStart(),
-            segment.getInterval().getEnd()
-        ),
-        segment.getVersion(),
-        segment.getShardSpec().getPartitionNum()
-    );
-
-    File indexFilesDir = file;
-
-    long indexSize = 0;
-    final File zipOutFile = File.createTempFile("druid", "index.zip");
-    ZipOutputStream zipOut = null;
+  @Override public DataSegment push(  File file,  DataSegment segment) throws IOException {
+    log.info("Uploading [%s] to S3",file);
+    String outputKey=JOINER.join(config.getBaseKey().isEmpty() ? null : config.getBaseKey(),segment.getDataSource(),String.format("%s_%s",segment.getInterval().getStart(),segment.getInterval().getEnd()),segment.getVersion(),segment.getShardSpec().getPartitionNum());
+    File indexFilesDir=file;
+    long indexSize=0;
+    final File zipOutFile=File.createTempFile("druid","index.zip");
+    ZipOutputStream zipOut=null;
     try {
-      zipOut = new ZipOutputStream(new FileOutputStream(zipOutFile));
-      File[] indexFiles = indexFilesDir.listFiles();
-      for (File indexFile : indexFiles) {
-        log.info("Adding indexFile[%s] with size[%,d].  Total size[%,d]", indexFile, indexFile.length(), indexSize);
+      zipOut=new ZipOutputStream(new FileOutputStream(zipOutFile));
+      File[] indexFiles=indexFilesDir.listFiles();
+      for (      File indexFile : indexFiles) {
+        log.info("Adding indexFile[%s] with size[%,d].  Total size[%,d]",indexFile,indexFile.length(),indexSize);
         if (indexFile.length() >= Integer.MAX_VALUE) {
-            throw new ISE("indexFile[%s] too large [%,d]", indexFile, indexFile.length());
+          throw new ISE("indexFile[%s] too large [%,d]",indexFile,indexFile.length());
         }
         zipOut.putNextEntry(new ZipEntry(indexFile.getName()));
-        IOUtils.copy(new FileInputStream(indexFile), zipOut);
-        indexSize += indexFile.length();
+        IOUtils.copy(new FileInputStream(indexFile),zipOut);
+        indexSize+=indexFile.length();
       }
     }
-    finally {
+  finally {
       Closeables.closeQuietly(zipOut);
     }
-
     try {
-      S3Object toPush = new S3Object(zipOutFile);
-
-      final String outputBucket = config.getBucket();
+      S3Object toPush=new S3Object(zipOutFile);
+      final String outputBucket=config.getBucket();
       toPush.setBucketName(outputBucket);
       toPush.setKey(outputKey + "/index.zip");
       toPush.setAcl(GSAccessControlList.REST_CANNED_BUCKET_OWNER_FULL_CONTROL);
-
-      log.info("Pushing %s.", toPush);
-      s3Client.putObject(outputBucket, toPush);
-
-      DataSegment outputSegment = segment.withSize(indexSize)
-                                         .withLoadSpec(
-                                             ImmutableMap.<String, Object>of(
-                                                 "type", "s3_zip",
-                                                 "bucket", outputBucket,
-                                                 "key", toPush.getKey()
-                                             )
-                                         );
-
-      File descriptorFile = File.createTempFile("druid", "descriptor.json");
-      StreamUtils.copyToFileAndClose(new ByteArrayInputStream(jsonMapper.writeValueAsBytes(segment)), descriptorFile);
-      S3Object descriptorObject = new S3Object(descriptorFile);
+      log.info("Pushing %s.",toPush);
+      s3Client.putObject(outputBucket,toPush);
+      DataSegment outputSegment=segment.withSize(indexSize).withLoadSpec(ImmutableMap.<String,Object>of("type","s3_zip","bucket",outputBucket,"key",toPush.getKey()));
+      File descriptorFile=File.createTempFile("druid","descriptor.json");
+      StreamUtils.copyToFileAndClose(new ByteArrayInputStream(jsonMapper.writeValueAsBytes(segment)),descriptorFile);
+      S3Object descriptorObject=new S3Object(descriptorFile);
       descriptorObject.setBucketName(outputBucket);
       descriptorObject.setKey(outputKey + "/descriptor.json");
       descriptorObject.setAcl(GSAccessControlList.REST_CANNED_BUCKET_OWNER_FULL_CONTROL);
-
-      log.info("Pushing %s", descriptorObject);
-      s3Client.putObject(outputBucket, descriptorObject);
-
-      log.info("Deleting Index File[%s]", indexFilesDir);
+      log.info("Pushing %s",descriptorObject);
+      s3Client.putObject(outputBucket,descriptorObject);
+      log.info("Deleting Index File[%s]",indexFilesDir);
       FileUtils.deleteDirectory(indexFilesDir);
-
-      log.info("Deleting zipped index File[%s]", zipOutFile);
+      log.info("Deleting zipped index File[%s]",zipOutFile);
       zipOutFile.delete();
-
-      log.info("Deleting descriptor file[%s]", descriptorFile);
+      log.info("Deleting descriptor file[%s]",descriptorFile);
       descriptorFile.delete();
-
       return outputSegment;
     }
-    catch (NoSuchAlgorithmException e) {
+ catch (    NoSuchAlgorithmException e) {
       throw new IOException(e);
     }
-    catch (S3ServiceException e) {
+catch (    S3ServiceException e) {
       throw new IOException(e);
     }
+  }
+  public S3SegmentPusher(){
   }
 }
